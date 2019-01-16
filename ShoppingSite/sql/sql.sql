6b267698b41173ae06c05d33805ec91dc322b712
KazumaSakai <mail@shidenvip.jp>
2019-01-16 09:56:37 +0900

    [update] Update sql.sql. Create 'ShoppingSite' Database. Create 'users' table

---
+++ ShoppingSite/sql/sql.sql
@@ -0,0 +1,16 @@
+set names utf8;
+set foreign_key_checks = 0;
+drop database if exists ShoppingSite;
+
+create database if not exists ShoppingSite;
+use ShoppingSite;
+
+drop table if exists users;
+
+create table users(
+	id int not null primary key auto_increment,
+	login_id varchar(16) unique,
+	login_pass varchar(60),
+	user_name varchar(60),
+	insert_date datetime
+);
